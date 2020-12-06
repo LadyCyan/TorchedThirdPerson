1b0045708fc6cc0b84e50bd0df7a573f31c94fa8
LadyCyan <taylorrenner@u.boisestate.edu>
2020-12-06 01:18:16 -0700

    Battery Collector Main

---
+++ Source/BatteryCollector/BatteryCollectorGameMode.h
@@ -0,0 +1,27 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BatteryCollectorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABatteryCollectorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABatteryCollectorGameMode();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	//rate at which the character loses power
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power")
+	float DecayRate;
+
+};
+
+
+
